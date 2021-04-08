9417b812be07074f16408d3f9a42db1cadfc4975
2116122-DanielBurke <81285642+2116122-DanielBurke@users.noreply.github.com>
2021-04-08 12:53:18 +0200

    Sorted pdfs into folder, added build and project for MicroProject3

---
+++ Projects/Microproject3_LevelDesign_216122_Daniel Burke/Assets/Scripts/Player/Player_Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 5;
+
+    public Transform movePoint;
+
+    public LayerMask tilemap_col;
+    private void Start()
+    {
+        movePoint.parent = null;
+    }
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        {
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, tilemap_col))
+                {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+                
+            }
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Vertical"), 0f, 0f), .2f, tilemap_col))
+                {
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
+            }
+        }
+       
+    }
+}
---
+++ Projects/Microproject3_LevelDesign_216122_Daniel Burke/Assets/Scripts/Player/Player_Unit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Unit : MonoBehaviour
+{
+
+    public static int currHealth, maxHealth;
+    public static int currLevel;
+    public static int currXP;
+    public static int maxHP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
