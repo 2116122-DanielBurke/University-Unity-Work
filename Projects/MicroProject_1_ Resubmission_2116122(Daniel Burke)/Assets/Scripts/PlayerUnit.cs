a6a2e974ba80a26e61769559745b1fd7ad1590c6
2116122-DanielBurke <81285642+2116122-DanielBurke@users.noreply.github.com>
2021-04-22 22:44:15 +0200

    Retrospective Project Plan

---
+++ Projects/MicroProject_1_ Resubmission_2116122(Daniel Burke)/Assets/Scripts/PlayerUnit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUnit : MonoBehaviour
+{
+    public string name;
+
+    public int currHp;
+    public int maxHP;
+
+    public int currPotion;
+    public int maxPotion;
+
+    public int curSkill01;
+    public int maxSkill01;
+
+    public int curSkill02;
+    public int maxSkill02;
+
+    public int curSkill03;
+    public int maxSkill03;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
